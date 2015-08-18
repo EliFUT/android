3fd67777667c3546fb6b64a1bb341b5722c809fd
Felipe Lima <felipe.lima@gmail.com>
2015-08-18 11:51:50 -0300

    Remove ruby scripts

--- scripts/images/leagues.rb
+++
@@ -1,7 +0,0 @@
-require 'open-uri'
-
-leagues = [1, 4, 10, 13, 14, 16, 17, 19, 20, 31, 32, 39, 41, 50, 53, 54, 56, 60, 61, 65, 66, 67, 68, 76, 78, 80, 83, 189, 308, 335, 336, 341, 350, 351, 353, 382, 383, 384, 2000, 2028]
-
-leagues.each do |id|
-  `curl https://fifa15.content.easports.com/fifa/fltOnlineAssets/8D941B48-51BB-4B87-960A-06A61A62EBC0/2015/fut/items/images/leagueLogos_sm/web/l#{id}.png > app/src/main/res/drawable-xhdpi/league_#{id}.png`
-end
--- scripts/players.rb
+++
@@ -1,6 +0,0 @@
-require 'open-uri'
-
-total_pages = 731
-1.upto(total_pages) do |page|
-	`curl https://www.easports.com/fifa/ultimate-team/api/fut/item?jsonParamObject=%7B%22page%22:#{page}%7D > scripts/players_#{page}.json`
-end

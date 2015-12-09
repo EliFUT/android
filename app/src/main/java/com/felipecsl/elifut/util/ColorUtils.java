91e8d86ef9516c693bee96983ccf497c4404dfdc
Felipe Lima <felipe.lima@gmail.com>
2015-12-08 21:11:08 -0800

    Use 10% darker color for system status bar tinting

--- app/src/main/java/com/felipecsl/elifut/util/ColorUtils.java
+++ app/src/main/java/com/felipecsl/elifut/util/ColorUtils.java
@@ -2,6 +2,7 @@ package com.felipecsl.elifut.util;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Build;
 import android.support.design.widget.TabLayout;
 import android.support.v7.widget.Toolbar;
@@ -17,7 +18,20 @@ public final class ColorUtils {
     tabs.setBackgroundColor(primaryColor);
     tabs.setSelectedTabIndicatorColor(secondaryColor);
     if (AndroidVersion.isAtLeastLollipop()) {
-      activity.getWindow().setStatusBarColor(primaryColor);
+      activity.getWindow().setStatusBarColor(darken(primaryColor, 0.9f));
     }
+  }
+
+  /** Returns darker version of specified color */
+  public static int darken(int color, float factor) {
+    int a = Color.alpha(color);
+    int r = Color.red(color);
+    int g = Color.green(color);
+    int b = Color.blue(color);
+
+    return Color.argb(a,
+        Math.max((int) (r * factor), 0),
+        Math.max((int) (g * factor), 0),
+        Math.max((int) (b * factor), 0));
   }
 }

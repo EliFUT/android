8f1173473ace79bed36355d9ce38ec84b2b5bb1a
Felipe Lima <felipe.lima@gmail.com>
2016-02-15 10:41:01 -0800

    Adds Google Analytics tracking

--- app/src/test/java/com/felipecsl/elifut/TestElifutComponent.java
+++ app/src/test/java/com/felipecsl/elifut/TestElifutComponent.java
@@ -5,7 +5,12 @@ import javax.inject.Singleton;
 import dagger.Component;
 
 @Singleton
-@Component(modules = { TestNetworkModule.class, TestDataModule.class, TestElifutModule.class })
+@Component(modules = {
+    TestNetworkModule.class,
+    TestDataModule.class,
+    TestElifutModule.class,
+    TestAnalyticsModule.class
+})
 public interface TestElifutComponent extends ElifutComponent, TestElifutGraph {
   /** An initializer that creates the graph from an application. */
   final class Initializer {
@@ -14,6 +19,7 @@ public interface TestElifutComponent extends ElifutComponent, TestElifutGraph {
           .testElifutModule(new TestElifutModule())
           .testNetworkModule(new TestNetworkModule(app))
           .testDataModule(new TestDataModule(app))
+          .testAnalyticsModule(new TestAnalyticsModule(app))
           .build();
     }
 
